a9f92c2b37789ee9399047f9827c19a7b29a96e6
Genics85 <eugeneamo85@gmail.com>
2022-01-14 22:37:29 +0000

    Firbase integration initiated

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAFcHrSrEERXlxBs7UvyxHLnDKgiJFurFA",
+    authDomain: "facebook-clone-db65b.firebaseapp.com",
+    projectId: "facebook-clone-db65b",
+    storageBucket: "facebook-clone-db65b.appspot.com",
+    messagingSenderId: "611878492673",
+    appId: "1:611878492673:web:19875cd85784c91150fe00",
+    measurementId: "G-RWTEVHKVBR"
+  };
